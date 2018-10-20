abc96da03f609d1d89e698aa19b07effd9049e66
Desnos <maximilien.desnos@epitech.eu>
2018-10-20 22:57:05 +0200

    add meteo finish and first steam

--- app/dashboard.js
+++ app/dashboard.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
+const $ = require('jquery')
 
 
 const apiKey = '9cb9ebf1faf9432efdd2489ed4b4f914';
+const apiKey_Steam = 'B80BA570C678C23C56E3B4F42CC90FBF'
 
 router.get('/', isLoggedIn, function (req, res) {
-    res.render('dashboard', {weather: null, error: null});
+    res.render('dashboard', {weather: null, weatherWeek: null, error: null});
 });
 
 // route middleware to make sure a user is logged in
@@ -20,24 +22,72 @@ function isLoggedIn(req, res, next) {
     res.redirect('/');
 }
 
-router.post('/', function (req, res) {
-    let city = req.body.city;
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-    request(url, function (err, response, body) {
-        if (err) {
-            res.render('dashboard', {weather: null, error: 'Error, please try again'})
-        } else {
-            let weather = JSON.parse(body);
-            console.log(weather);
-            if (weather.main === undefined) {
-                res.render('dashboard', {weather: null, error: 'Error, please try again'})
+function getRequest(url, res) {
+    return new Promise(function (success, failure) {
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log("Success");
+                success(body);
             } else {
-                let weatherText = `Il fait ${weather.main.temp}° à ${weather.name}`;
-                res.render('dashboard', {weather: weatherText, error: null})
+                console.log("Error");
+                res.render('dashboard', {weather: null, weatherWeek: null, error: 'Error, please try againnn'})
+                // failure(error);
             }
-        }
-    })
-});
+        });
+    });
+}
+
+router.post('/currentMeteo', function (req, res) {
+        let city = req.body.city;
+        let url_current_weather = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+        let url_weather_five_days = `http://api.openweathermap.org/data/2.5/forecast?q=${city},fr&units=metric&appid=${apiKey}`
+        let url_steam_info = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey_Steam}&steamids=76561198068118015`
+        let weatherText = [];
+        let weatherWeekText = [];
+        getRequest(url_current_weather, res).then(function (body1) {
+            let weather_current = JSON.parse(body1);
+            if (weather_current.main === undefined) {
+                res.render('dashboard', {weather: null, weatherWeek: null, error: 'Error, please try again'})
+            } else {
+                cityGlob = city;
+                weatherText = []
+                weatherText.push( `Temp: ${weather_current.main.temp}°`);
+                weatherText.push( `Sky : ${weather_current.weather[0].description}`);
+                weatherText.push( `Temp max: ${weather_current.main.temp_max}°`);
+                weatherText.push( `Temp min: ${weather_current.main.temp_min}°`);
+                weatherText.push( `Humidity: ${weather_current.main.humidity}%`);
+                return getRequest(url_weather_five_days, res);
+            }}).then(function (body2) {
+            let weather_prevision = JSON.parse(body2);
+            let my_list = [];
+            my_list.push(weather_prevision.list);
+            let second_list = my_list.pop();
+            let size_json = second_list.length;
+            let i = 0;
+            let today = Date.now();
+            let T = new Date(today);
+            let tmp = null;
+            while (size_json != i) {
+                tmp = new Date(weather_prevision.list[i].dt_txt)
+                let dayCurrent = [];
+                if ((T.getDay() - tmp.getDay()) != 0 && tmp.getHours() == 12) {
+                    dayCurrent = [];
+                    dt = new Date(weather_prevision.list[i].dt_txt);
+                    dayCurrent.push(dt.getDate() + "/" + dt.getMonth() +
+                        ": Temp = " + weather_prevision.list[i].main.temp +
+                        "; Sky = " + weather_prevision.list[i].weather[0].description +
+                        "; Humidity = " + weather_prevision.list[i].main.humidity);
+                }
+                weatherWeekText.push(dayCurrent);
+                i++;
+            }
+            return getRequest(url_steam_info, res);
+        }).then(function (body3) {
+            let steam_info = JSON.parse(body3);
+            res.render('dashboard', {weather: weatherText, weatherWeek: weatherWeekText, error: null});
+        })
+    }
+);
 
 router.get('/logout', function (req, res) {
     req.logout();
