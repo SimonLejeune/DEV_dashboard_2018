4beb0f1923d02ffa4a6e538cb11c9f1c57f054bd
Desnos <maximilien.desnos@epitech.eu>
2018-10-20 23:24:05 +0200

    add jquer package.json

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname+"/public"));
+
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
