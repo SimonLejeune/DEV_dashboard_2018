40366c0038c942a177d3e3b30e9db6e27349cfd6
Desnos <maximilien.desnos@epitech.eu>
2018-10-21 18:12:50 +0200

    steam and weather interface

--- app/dashboard.js
+++ app/dashboard.js
@@ -7,9 +7,13 @@ const $ = require('jquery')
 const apiKey = '9cb9ebf1faf9432efdd2489ed4b4f914';
 const apiKey_Steam = 'B80BA570C678C23C56E3B4F42CC90FBF';
 const id_default = '76561198068118015';
+var CurrentWeather = null;
+var PrevisionWeather = null;
+var steamInfo = null;
+var steamGameList = null;
 
 router.get('/', isLoggedIn, function (req, res) {
-    res.render('dashboard', {weather: null, weatherWeek: null, error: null});
+    res.render('dashboard', {weather: null, weatherWeek: null, steamInfo: null, GameList: null, error: null});
 });
 
 // route middleware to make sure a user is logged in
@@ -31,12 +35,48 @@ function getRequest(url, res) {
                 success(body);
             } else {
                 console.log("Error");
-                res.render('dashboard', {weather: null, weatherWeek: null, error: 'Error, please try againnn'})
+                res.render('dashboard', {weather: CurrentWeather, weatherWeek: PrevisionWeather, steamInfo: steamInfo, GameList: steamGameList, error: 'Error, please try againnn'})
                 // failure(error);
             }
         });
     });
 }
+
+router.post('/InfoSteam',  function (req, res) {
+    id = req.body.id;
+    console.log(id);
+    let url_steam_info = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey_Steam}&steamids=${id}`;
+    let url_get_game =  ` http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${apiKey_Steam}&steamid=${id}&format=json`;
+    let steamText = [];
+    getRequest(url_steam_info, res).then(function (body1) {
+        let steam_info = JSON.parse(body1);
+        if (steam_info.response.players.length == 0)
+            res.render('dashboard', {weather: CurrentWeather, weatherWeek: PrevisionWeather, steamInfo: steam_info, GameList: steamGameList, error: 'Error, please try againnn'})
+        else
+          //  steamText = steam_info.response.players[0].avatarfull;
+            steamText.push(steam_info.response.players[0].avatarfull);
+            steamText.push(steam_info.response.players[0].personaname);
+            if (steam_info.response.players[0].personastate == 0)
+                steamText.push("disconnected");
+            else
+                steamText.push("connected");
+            steamText.push(steam_info.response.players[0].profileurl)
+            steamInfo = steamText;
+            return getRequest(url_get_game, res);
+
+    }).then(function (body2) {
+        let steam_games = JSON.parse(body2);
+        let games_list = [];
+    //    while (steam_games.response)
+        var i = 0;
+        while (i != steam_games.response.games.length) {
+            games_list.push(steam_games.response.games[i].name);
+            i++;
+        }
+        steamGameList = games_list;
+        res.render('dashboard', {weather: CurrentWeather, weatherWeek: PrevisionWeather, steamInfo: steamInfo, GameList: steamGameList, error: null});
+        })
+});
 
 router.post('/currentMeteo', function (req, res) {
         let city = req.body.city;
@@ -44,13 +84,12 @@ router.post('/currentMeteo', function (req, res) {
             city = "lille";
         let url_current_weather = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
         let url_weather_five_days = `http://api.openweathermap.org/data/2.5/forecast?q=${city},fr&units=metric&appid=${apiKey}`
-        let url_steam_info = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey_Steam}&steamids=${id_default}`
         let weatherText = [];
         let weatherWeekText = [];
         getRequest(url_current_weather, res).then(function (body1) {
             let weather_current = JSON.parse(body1);
             if (weather_current.main === undefined) {
-                res.render('dashboard', {weather: null, weatherWeek: null, error: 'Error, please try again'})
+                res.render('dashboard', {weather: null, weatherWeek: null, steamInfo: null, GameList: null, error: 'Error, please try again'})
             } else {
                 cityGlob = city;
                 weatherText = []
@@ -84,10 +123,9 @@ router.post('/currentMeteo', function (req, res) {
                 weatherWeekText.push(dayCurrent);
                 i++;
             }
-            return getRequest(url_steam_info, res);
-        }).then(function (body3) {
-            let steam_info = JSON.parse(body3);
-            res.render('dashboard', {weather: weatherText, weatherWeek: weatherWeekText, error: null});
+            CurrentWeather = weatherText;
+            PrevisionWeather = weatherWeekText;
+            res.render('dashboard', {weather: weatherText, weatherWeek: weatherWeekText, steamInfo:steamInfo, GameList: steamGameList,  error: null});
         })
     }
 );
